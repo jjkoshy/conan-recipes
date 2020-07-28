40517f0a0eb3dd3ac66900b3428fe960e06d5e7c
Joel Koshy <jkoshy@linkedin.com>
2020-07-28 14:30:41 -0700

    cleanups; disable searching in system paths

--- conan-brpc/conanfile.py
+++ conan-brpc/conanfile.py
@@ -30,7 +30,6 @@ class BrpcConan(ConanFile):
         # conan create ... -o protobuf:shared=True
         self.options['gflags'].shared = True
         self.options['gflags'].nothreads = False
-        # TODO figure out if we need with_ options for zlib
         self.options['protobuf'].with_zlib = True
         self.options['protobuf'].shared = True
         self.options['leveldb'].with_snappy = self.options.with_snappy
@@ -55,19 +54,6 @@ class BrpcConan(ConanFile):
 
     def configure_cmake(self):
         cmake = CMake(self)
-        # uncomment for non-parallel builds
-        # cmake.parallel = False
-
-        # The cmake_paths generator produces conan_paths.cmake which sets
-        # various path variables for the libraries this recipe depends on, and
-        # adds those to the CMAKE_MODULE_PATH (used by include() and
-        # FindXXX.cmake) and CMAKE_PREFIX_PATH (used by find_library and
-        # find_dependency). For e.g.:
-        #
-        # set(CONAN_ZLIB_ROOT "/home/jjkoshy/.conan/data/zlib/1.2.11/conan/stable/package/7c292f54b7e6c224f121c640404bf7fbbeb41a8f")
-        # set(CMAKE_MODULE_PATH "/home/jjkoshy/.conan/data/brpc/0.9.5/jjkoshy/stable/package/3a1b935ee508ae45a4e5b45eb2504048debd5203/"
-        #                       "/home/jjkoshy/.conan/data/gflags/2.2.2/bincrafters/stable/package/b79e8887815d0c32512f737644747a0a383a2545/"
-        # ... )
 
         cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "conan_paths.cmake"
         # TODO: the source_subfolder should be something generic like
